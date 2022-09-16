5ae5c40b8ca4dd138dd85d394e0ad8a1a5ea4abd
enes <enesooo78@gmail.com>
2022-09-17 00:26:18 +0300

    çalışmalar

---
+++ Netflix/app.js
@@ -0,0 +1,5 @@
+document.getElementById("div1").style.backgroundColor = "green";
+document.getElementsByTagName("h1")[0].textContent = "GOKTAS";
+document.querySelectorAll("p")[1].id = "paragraf";
+
+console.log(document.querySelectorAll("p"));
