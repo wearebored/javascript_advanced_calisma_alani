dbee69bc76c9046d8bdb57ecd27134917ae67523
enes <enesooo78@gmail.com>
2022-09-19 01:20:02 +0300

    kodlama hazır

--- js15-dom-project-example/game.js
+++ js15-dom-project-example/game.js
@@ -1,105 +1,63 @@
-//* =====================================
-//*  GUESS MY NUMBER
-//*  Game Logic
-//*======================================
-
-//? 1-100 arasinda rasgele bir sayi tut.
-let randomNumber = Math.round(Math.random() * 100);
-console.log(randomNumber);
-
-//? Variables
 let score = 10;
-// let topScore = 0;
+let number = Number((Math.random() * 100).toFixed());
+console.log(number);
 
-//? localStorage'de topScore adiyla bir degisken olustur.
-let topScore = localStorage.getItem("topScore") || 0;
+const inpt = document.querySelector("input");
+const button = document.querySelector(".check-btn");
+const again = document.querySelector(".again-btn");
+const yazı = document.querySelector(".msg");
+console.log(button);
+window.onload = function () {
+  inpt.focus();
+};
+button.onclick = function () {
+  if (inpt.value=="") {
+    yazı.textContent = "Please enter a number";
+  } else if (Number(inpt.value) > 100) {
+    yazı.textContent = `${inpt.value} Sayısı 100'den Büyük.`;
+    score -= 1;
+  } else if (Number(inpt.value) < number) {
+    yazı.textContent = `${inpt.value} Sayısından Büyük.`;
+    score -= 1;
+  } else if (Number(inpt.value) > number) {
+    yazı.textContent = `${inpt.value} Sayısından Küçük.`;
 
-//? DOM'daki top-score degerini localStorage'den okuyarak guncelle.
-document.querySelector(".top-score").textContent = topScore;
-
-//* CheckBtn basildiginda kontrolleri yap
-document.querySelector(".check-btn").addEventListener("click", () => {
-  const guessInput = Number(document.querySelector(".guess-input").value);
-  const msg = document.querySelector(".msg");
-  const body = document.querySelector("body");
-
-  //? eger input girilmediyse Kullaniciya uyari ver.
-  if (!guessInput) {
-    msg.innerText = "Please enter a number";
-    //! eger rasgele == input.value
-  } else if (randomNumber === guessInput) {
-    msg.innerHTML = `Congrats You Win <i class="fa-solid fa-face-grin-hearts fa-2x"></i> `;
-    body.className = "bg-success";
-    document.querySelector(".check-btn").disabled = true;
-    if (score > topScore) {
-      // topScore = score;
-
-      //? localStorage'deki topScore degiskenini guncelle
-      localStorage.setItem("topScore", score);
-      //? DOM'daki top-score degerini guncelle
-      document.querySelector(".top-score").textContent = score;
-    }
-    document.querySelector(".secret-number").textContent = randomNumber;
-
-    //! eger rasgele!= input.value
-  } else {
-    score--;
-    if (score > 0) {
-      guessInput > randomNumber
-        ? (msg.innerHTML = `<i class="fa-solid fa-arrow-trend-down fa-2x"></i> DECREASE `)
-        : (msg.innerHTML = `<i class="fa-solid fa-arrow-trend-up fa-2x"></i> INCREASE `);
-    } else {
-      msg.innerHTML = `You Lost <i class="fa-regular fa-face-sad-tear fa-2x"></i>`;
-      document.querySelector(".secret-number").textContent = randomNumber;
-      body.className = "bg-danger";
-      document.querySelector(".check-btn").disabled = true;
-    }
-
-    document.querySelector(".score").textContent = score;
+    score -= 1;
+  } else if (Number(inpt.value) === number) {
+    yazı.textContent = "Kazandınız.";
+    document.querySelector(".top-score").textContent = score;
+    button.disabled = true;
+    document.querySelector("body").style.background = "green";
+    document.querySelector(".secret-number").textContent = number;
+    inpt.disabled = true;
   }
-});
-
-//* again basildiginda oyunu baslangic dgerlerin kur
-document.querySelector(".again-btn").addEventListener("click", () => {
+  document.querySelector(".score").textContent = score;
+  if (!score) {
+    yazı.textContent = "Kaybettin.";
+    button.disabled = true;
+    document.querySelector("body").style.background = "red";
+    document.querySelector(".secret-number").textContent = number;
+    inpt.disabled = true;
+  }
+  inpt.value = "";
+  inpt.focus();
+};
+again.onclick = function () {
+  document.querySelector(".secret-number").textContent = "?";
   score = 10;
   document.querySelector(".score").textContent = score;
-  randomNumber = Math.round(Math.random() * 100);
-  document.querySelector(".secret-number").textContent = "?";
-  console.log(randomNumber);
-  document.querySelector(".check-btn").disabled = false;
-  document.querySelector("body").classList.remove("bg-success", "bg-danger");
-  document.querySelector(".guess-input").value = "";
-  document.querySelector(".msg").innerText = `Starting..`;
-});
-
-document.querySelector(".guess-input").addEventListener("keydown", (e) => {
-  if (e.code === "Enter") {
-    document.querySelector(".check-btn").click();
+  inpt.value = "";
+  document.querySelector(".top-score").textContent = 0;
+  yazı.textContent = "Starting..";
+  inpt.disabled = false;
+  button.disabled = false;
+  document.querySelector("body").style.background = "#2d3436";
+  inpt.focus();
+  number = Math.round(Math.random() * 100);
+  console.log(number);
+};
+inpt.onkeyup = function (tuş) {
+  if (tuş.code === "Enter") {
+    button.onclick();
   }
-});
-
-//! LOCALSTORAGE- SESSIONSTORAGE
-// myObj = { a: 1, b: 2, c: 3 };
-// localStorage.setItem("OBJ", JSON.stringify(myObj));
-// const readObj = localStorage.getItem("OBJ");
-// const readOBJ = JSON.parse(localStorage.getItem("OBJ"));
-// console.log(typeof readObj);
-// console.log(typeof readOBJ);
-// console.log(readOBJ);
-
-//* PUSEDUO
-//? eger score > topScore
-//?     topScore = score
-//? secret_number = gorunur.
-
-//! değilse
-//! eger score > 0
-//!   score = score -1
-//?   eğer rasgele < input.value
-//?     AZALT
-//?   degilse
-//?     ARTTIR
-//! degise
-//? Uzgunuz kaybetiniz.
-
-//* againBtn basildiginda kontrolleri yap
+};
