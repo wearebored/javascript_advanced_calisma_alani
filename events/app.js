a0c984c9d322baec401f6b82ce2735e029504dc1
enes <enesooo78@gmail.com>
2022-09-18 16:07:10 +0300

    tamam

--- events/app.js
+++ events/app.js
@@ -1,70 +1,78 @@
 console.log("**** app.js *******");
 
-//? Selectors
+// Selectors
 const langInput = document.querySelector(".lang-input");
 const addBtn = document.querySelector("#add-btn");
 const deleteBtn = document.querySelector("#delete-btn");
 const langList = document.getElementById("lang-list");
 
+const pl = document.querySelector("h1");
+pl.onmouseover = function () {
+  pl.style.color = "black";
+};
+pl.onmouseout = function () {
+  pl.style.color = "red";
+};
+console.log(pl.textContent);
+pl.onclick = function () {
+  pl.style.backgroundColor = "green";
+  pl.style.fontWeight = "900";
+};
 
+addBtn.onclick = function () {
+  if (langInput.value === "") {
+    alert("hiçbirşey girilmedi.");
+  } else {
+    alert("okey");
+  }
+  
+};
+console.log(addBtn);
 
+// -----------------------
 
+const newUl = document.createElement("ul");
+langList.appendChild(newUl);
 
+//? addBtn event handler
+addBtn.addEventListener("click", () => {
+  if (!langInput.value) {
+    alert("Please enter a language");
+  } else {
+    newUl.innerHTML += `<li>${langInput.value}</li>`;
+    langInput.value = "";
+  }
+  langInput.focus();
+});
 
+//? deleteBtn event handler
+deleteBtn.addEventListener("click", () => {
+  newUl.childElementCount > 0
+    ? newUl.removeChild(newUl.lastElementChild)
+    : alert("There is no item to delete");
+});
 
+//? enter key and  del key event handler
+langInput.addEventListener("keydown", (event) => {
+  // console.log(event);
+  // console.log(event.target);
+  // console.log(event.keyCode);
+  // console.log(event.code);
 
+  if (event.keyCode === 13) {
+    addBtn.click();
+  }
 
+  // if (event.code === "Delete") {
+  //   deleteBtn.click();
+  // }
 
+  if (event.keyCode === 46) {
+    deleteBtn.click();
+  }
+});
 
-
-
-
-
-
-
-
-// const newUl = document.createElement("ul");
-// langList.appendChild(newUl);
-
-// //? addBtn event handler
-// addBtn.addEventListener("click", () => {
-//   if (!langInput.value) {
-//     alert("Please enter a language");
-//   } else {
-//     newUl.innerHTML += `<li>${langInput.value}</li>`;
-//     langInput.value = "";
-//   }
-//   langInput.focus();
-// });
-
-// //? deleteBtn event handler
-// deleteBtn.addEventListener("click", () => {
-//   newUl.childElementCount > 0
-//     ? newUl.removeChild(newUl.lastElementChild)
-//     : alert("There is no item to delete");
-// });
-
-// //? enter key and  del key event handler
-// langInput.addEventListener("keydown", (event) => {
-//   // console.log(event);
-//   // console.log(event.target);
-//   // console.log(event.keyCode);
-//   // console.log(event.code);
-
-//   if (event.keyCode === 13) {
-//     addBtn.click();
-//   }
-
-//   // if (event.code === "Delete") {
-//   //   deleteBtn.click();
-//   // }
-
-//   if (event.keyCode === 46) {
-//     deleteBtn.click();
-//   }
-// });
-
-// //? onload event handler
-// window.addEventListener("load", () => {
-//   langInput.focus();
-// });
+//? onload event handler
+window.addEventListener("load", () => {
+  langInput.focus();
+});
