09ebaa00f337f9bf762f576f4109b5139b64aaa4
enes <enesooo78@gmail.com>
2022-09-22 21:21:31 +0300

    hazır

--- checkout_page/app.js
+++ checkout_page/app.js
@@ -1,20 +1,20 @@
 const satıs = document.querySelector(".satıs");
-// const adet = document.querySelector(konum).textContent
-// const adet = document.querySelector(".ekle_cıkar >div p").textContent
+let shi = 15;
 let ürünler = {
   ürün1: 55,
   ürün2: 44,
   ürün3: 22,
-  ürün4:33
+  ürün4: 33,
+  ürün5: 11,
 };
+let nakliye = 15;
 window.onload = function (e) {
   let eleman_say =
     e.target.doctype.nextSibling.children[1].children[1].children[1].children;
+
   for (i of eleman_say) {
-    console.log(i);
+    let bosluk = i.className.indexOf(" ");
     if (i.className.indexOf("ürün") == 0) {
-      let bosluk = i.className.indexOf(" ");
-      console.log(i.className.slice(0, bosluk));
       document.querySelector(
         `.${i.className.slice(0, bosluk)} .fiyat p`
       ).textContent = `$${ürünler[i.className.slice(0, bosluk)]}`;
@@ -23,53 +23,120 @@ window.onload = function (e) {
       ).textContent = `$${(ürünler[i.className.slice(0, bosluk)] * 1.1).toFixed(
         2
       )}`;
+    } else if (i.className.indexOf("total") == 0) {
+      let prod = 0;
+
+      for (i of eleman_say) {
+        if (i.className.indexOf("ürün") == 0) {
+          let fiyat =
+            i.children[1].children[0].children[1].children[0].innerText;
+          let adet =
+            i.children[1].children[1].children[0].children[1].innerText;
+          if (fiyat.slice(0, 1) == "$") {
+            let bosluk = i.className.indexOf(" ");
+            prod += Number(fiyat.slice(1) * adet);
+            document.querySelector(
+              `.${i.className.slice(0, bosluk)} .ekle_cıkar span`
+            ).textContent = `${Number(fiyat.slice(1) * adet)}`;
+          }
+        }
+      }
+      const sub = document.querySelector(`.${i.className} .subtotal span`);
+      const tax = document.querySelector(`.${i.className} .tax span`);
+      const ship = document.querySelector(`.${i.className} .shipping span`);
+      const tot = document.querySelector(`.${i.className} .totals span`);
+      sub.textContent = `$${prod.toFixed(2)}`;
+      tax.textContent = `$${(prod * 0.18).toFixed(2)}`;
+      ship.textContent = `$${shi}`;
+      tot.textContent = `$${(
+        Number(prod) +
+        Number(prod * 0.18) +
+        Number(shi)
+      ).toFixed(2)}`;
+      
     }
   }
-
-  console.log(
-    e.target.doctype.nextSibling.children[1].children[1].children[1].children
-  );
 };
-// console.log(adet);
+
 satıs.onclick = function (e) {
   let adet = e.path[1].children[1].innerText;
   let konum = "." + e.path[2].classList[1];
   let konumlar = document.querySelector(`${konum} div p`);
-  let carp_fiyat = document.querySelector(`${konum} .prod span`);
-  let tik = e.path[0].innerText;
-  let ana_fiyat =
-    "." +
-    e.path[3].firstChild.parentElement.children[0].lastElementChild
-      .classList[1];
-  let ana_fiyatlar = document.querySelector(ana_fiyat);
 
-  //   let dene =
-  //     e.path[3].attributes[0].ownerElement.childNodes[1].children[1].firstChild
-  //       .textContent;
-  console.log(e);
+  let tik = e.path[0].innerText;
+  let eleman_say =
+    e.view.document.children[0].children[1].children[1].children[1].children;
+
   if (tik == "-") {
     if (adet > 1) {
       konumlar.innerText--;
     } else if (adet == 1) {
-      console.log("satırı sil uyarısı");
+      let bosluk = e.path[4].className.indexOf(" ");
+      let conf = confirm("Ürünü listeden kaldırmak istiyormusunuz?");
+      if (conf) {
+        document
+          .querySelector(`.${e.path[4].className.slice(0, bosluk)}`)
+          .remove();
+      }
     }
   } else if (tik == "+") {
     konumlar.innerText++;
   } else if (tik == "Remove") {
-    console.log("satırı sil uyarısı");
+    
+    let bosluk = e.path[3].className.indexOf(" ");
+    let conf = confirm("Ürünü listeden kaldırmak istiyormusunuz?");
+    if (conf) {
+      document
+        .querySelector(`.${e.path[3].className.slice(0, bosluk)}`)
+        .remove();
+    }
   }
-  // let space_index = ana_fiyatlar.textContent.indexOf(" ");
 
-  // console.log(ana_fiyatlar.textContent);
-  // carp_fiyat.textContent = (
-  //   konumlar.textContent * ana_fiyatlar.textContent.slice(1, space_index - 1)
-  // ).toFixed(2);
   const prod2 = function (a) {
     return document.querySelector(a).innerText;
   };
-  // let ab = document.querySelector(".fiyat1").innerText;
-  // console.log(ab);
-  // for(i of [a,b,c]){
 
-  // }
+  for (i of eleman_say) {
+    let bosluk = i.className.indexOf(" ");
+    if (i.className.indexOf("ürün") == 0) {
+      document.querySelector(
+        `.${i.className.slice(0, bosluk)} .fiyat p`
+      ).textContent = `$${ürünler[i.className.slice(0, bosluk)]}`;
+      document.querySelector(
+        `.${i.className.slice(0, bosluk)} span`
+      ).textContent = `$${(ürünler[i.className.slice(0, bosluk)] * 1.1).toFixed(
+        2
+      )}`;
+    } else if (i.className.indexOf("total") == 0) {
+      let prod = 0;
+
+      for (i of eleman_say) {
+        if (i.className.indexOf("ürün") == 0) {
+          let fiyat =
+            i.children[1].children[0].children[1].children[0].innerText;
+          let adet =
+            i.children[1].children[1].children[0].children[1].innerText;
+          if (fiyat.slice(0, 1) == "$") {
+            let bosluk = i.className.indexOf(" ");
+            prod += Number(fiyat.slice(1) * adet);
+            document.querySelector(
+              `.${i.className.slice(0, bosluk)} .ekle_cıkar span`
+            ).textContent = `${Number(fiyat.slice(1) * adet)}`;
+          }
+        }
+      }
+      const sub = document.querySelector(`.${i.className} .subtotal span`);
+      const tax = document.querySelector(`.${i.className} .tax span`);
+      const ship = document.querySelector(`.${i.className} .shipping span`);
+      const tot = document.querySelector(`.${i.className} .totals span`);
+      sub.textContent = `$${prod.toFixed(2)}`;
+      tax.textContent = `$${(prod * 0.18).toFixed(2)}`;
+      ship.textContent = `$${shi}`;
+      tot.textContent = `$${(
+        Number(prod) +
+        Number(prod * 0.18) +
+        Number(shi)
+      ).toFixed(2)}`;
+    }
+  }
 };
