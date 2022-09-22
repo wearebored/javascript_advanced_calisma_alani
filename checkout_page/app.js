3d729912daf47c62a558cb7415858ee1cec5e13e
enes <enesooo78@gmail.com>
2022-09-22 18:29:55 +0300

    html ve css ufak düzeltmeler

--- checkout_page/app.js
+++ checkout_page/app.js
@@ -1,7 +1,35 @@
 const satıs = document.querySelector(".satıs");
 // const adet = document.querySelector(konum).textContent
 // const adet = document.querySelector(".ekle_cıkar >div p").textContent
+let ürünler = {
+  ürün1: 55,
+  ürün2: 44,
+  ürün3: 22,
+  ürün4:33
+};
+window.onload = function (e) {
+  let eleman_say =
+    e.target.doctype.nextSibling.children[1].children[1].children[1].children;
+  for (i of eleman_say) {
+    console.log(i);
+    if (i.className.indexOf("ürün") == 0) {
+      let bosluk = i.className.indexOf(" ");
+      console.log(i.className.slice(0, bosluk));
+      document.querySelector(
+        `.${i.className.slice(0, bosluk)} .fiyat p`
+      ).textContent = `$${ürünler[i.className.slice(0, bosluk)]}`;
+      document.querySelector(
+        `.${i.className.slice(0, bosluk)} span`
+      ).textContent = `$${(ürünler[i.className.slice(0, bosluk)] * 1.1).toFixed(
+        2
+      )}`;
+    }
+  }
 
+  console.log(
+    e.target.doctype.nextSibling.children[1].children[1].children[1].children
+  );
+};
 // console.log(adet);
 satıs.onclick = function (e) {
   let adet = e.path[1].children[1].innerText;
@@ -30,10 +58,18 @@ satıs.onclick = function (e) {
   } else if (tik == "Remove") {
     console.log("satırı sil uyarısı");
   }
-  let space_index = ana_fiyatlar.textContent.indexOf(" ");
+  // let space_index = ana_fiyatlar.textContent.indexOf(" ");
 
-  console.log(ana_fiyatlar.textContent);
-  carp_fiyat.textContent = (
-    konumlar.textContent * ana_fiyatlar.textContent.slice(1, space_index - 1)
-  ).toFixed(2);
+  // console.log(ana_fiyatlar.textContent);
+  // carp_fiyat.textContent = (
+  //   konumlar.textContent * ana_fiyatlar.textContent.slice(1, space_index - 1)
+  // ).toFixed(2);
+  const prod2 = function (a) {
+    return document.querySelector(a).innerText;
+  };
+  // let ab = document.querySelector(".fiyat1").innerText;
+  // console.log(ab);
+  // for(i of [a,b,c]){
+
+  // }
 };
