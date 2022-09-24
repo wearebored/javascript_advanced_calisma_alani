1f5bea1d7f2ed95c54ba0c86f77fe400a0b41273
enes <enesooo78@gmail.com>
2022-09-24 21:10:42 +0300

    çözüm

---
+++ kod_Bracket_Validator/app.js
@@ -0,0 +1,49 @@
+// console.log("object");
+// const ifade = "([]{[]})[]{{}()}";
+// const ac = ["(", "[", "{"];
+// const kapat = [")", "]", "}"];
+let ifade1 = " { [ ] ( ) }";
+let ifadeler = "";
+
+for (let i = 0; i < 9; i++) {
+  if (
+    ifade1.indexOf("[]") >= 0 ||
+    ifade1.indexOf("()") >= 0 ||
+    ifade1.indexOf("{}") >= 0
+  ) {
+    
+
+    for (let i = -1; a > i; i) {
+      ifadeler += ifade1.slice(0, a);
+      ifadeler += ifade1.slice(a + 2);
+      ifade1 = ifadeler;
+      ifadeler = "";
+      a = ifade1.indexOf("[]");
+      console.log(a);
+    }
+    for (let i = -1; b > i; i) {
+      ifadeler += ifade1.slice(0, b);
+      ifadeler += ifade1.slice(b + 2);
+      ifade1 = ifadeler;
+      ifadeler = "";
+      b = ifade1.indexOf("()");
+      console.log(b);
+    }
+    console.log(b);
+    for (let i = -1; c > i; i) {
+      ifadeler += ifade1.slice(0, c);
+      ifadeler += ifade1.slice(c + 2);
+      ifade1 = ifadeler;
+      ifadeler = "";
+      c = ifade1.indexOf("{}");
+      console.log(c);
+    }
+
+
+    console.log(ifade1);
+  }
+  else{
+    console.log("hatalı");
+    break
+  }
+}
---
+++ kod_Bracket_Validator/app2.js
@@ -0,0 +1,64 @@
+const kontrolcü = (e) => {
+  const ifade_girdi = ayırıcı(e);
+  let ifade = ifade_girdi;
+  let ifader_topla = "";
+  let a = ifade.indexOf("[]");
+  let b = ifade.indexOf("()");
+  let c = ifade.indexOf("{}");
+  while (a > -1 || b > -1 || c > -1) {
+    if (a >= 0) {
+      for (let i = -1; a > i; i) {
+        console.log(ifade);
+        ifader_topla += ifade.slice(0, a);
+        ifader_topla += ifade.slice(a + 2);
+        ifade = ifader_topla;
+        ifader_topla = "";
+        a = ifade.indexOf("[]");
+        b = ifade.indexOf("()");
+        c = ifade.indexOf("{}");
+      }
+    } else if (b >= 0) {
+      for (let i = -1; b > i; i) {
+        console.log(ifade);
+        ifader_topla += ifade.slice(0, b);
+        ifader_topla += ifade.slice(b + 2);
+        ifade = ifader_topla;
+        ifader_topla = "";
+        a = ifade.indexOf("[]");
+        b = ifade.indexOf("()");
+        c = ifade.indexOf("{}");
+      }
+    } else if (c >= 0) {
+      for (let i = -1; c > i; i) {
+        console.log(ifade);
+        ifader_topla += ifade.slice(0, c);
+        ifader_topla += ifade.slice(c + 2);
+        ifade = ifader_topla;
+        ifader_topla = "";
+        a = ifade.indexOf("[]");
+        b = ifade.indexOf("()");
+        c = ifade.indexOf("{}");
+      }
+    }
+  }
+  if (ifade == "") {
+    console.log(`${ifade_girdi} doğru yazılmıştır.`);
+  } else {
+    console.log(`${ifade_girdi} hatalı yazılmıştır.`);
+  }
+};
+
+function ayırıcı(a) {
+  let veri = "";
+  let isaret = ["(", "{", "[", ")", "}", "]"];
+  for (i of a) {
+    for (c of isaret) {
+      if (i == c) {
+        veri += i;
+      }
+    }
+  }
+  return veri;
+}
+
+kontrolcü("([]{[]})[]{{}()}");
