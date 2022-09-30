86ddf349733fe7ef4f52f012771ade47f2f10002
enes <enesooo78@gmail.com>
2022-09-30 23:09:35 +0300

    a

--- teamwork_30.09.2022/digital_clock/app1.js
+++ teamwork_30.09.2022/digital_clock/app1.js
@@ -7,41 +7,40 @@
 // let dakika = new Date().getMinutes();
 // let saniye = new Date().getSeconds();
 setInterval(()=>{
-let saat = new Date().getHours();
-let dakika = new Date().getMinutes();
-let saniye = new Date().getSeconds();
+  let saat = new Date().getHours();
+  let dakika = new Date().getMinutes();
+  let saniye = new Date().getSeconds();
 
-// document.querySelector(".saat").innerHTML = saat;
-// document.querySelector(".dakika").innerHTML = dakika;
-// document.querySelector(".saniye").innerHTML = saniye;
-// if(saat<10){
-//     document.querySelector(".saat").innerHTML ="0"+ saat;
-// }
-// else{
-//     document.querySelector(".saat").innerHTML = saat;
-// }
-saat < 10
-  ? (document.querySelector(".saat").innerHTML = "0" + saat)
-  : (document.querySelector(".saat").innerHTML = saat);
-  
-if (dakika < 10) {
-  document.querySelector(".dakika").innerHTML = "0" + dakika;
-} else {
-  document.querySelector(".dakika").innerHTML = dakika;
-}
-if (saniye < 10) {
-  document.querySelector(".saniye").innerHTML = "0" + saniye;
-} else {
-  document.querySelector(".saniye").innerHTML = saniye;
-}
-if(saat>12){
-    document.querySelector(".gece").innerHTML="PM"
-}
-else{
+  // document.querySelector(".saat").innerHTML = saat;
+  // document.querySelector(".dakika").innerHTML = dakika;
+  // document.querySelector(".saniye").innerHTML = saniye;
+  // if(saat<10){
+  //     document.querySelector(".saat").innerHTML ="0"+ saat;
+  // }
+  // else{
+  //     document.querySelector(".saat").innerHTML = saat;
+  // }
+  // saat < 10 || (document.querySelector(".saat").innerHTML = "0" + saat)
+  // saat < 10 && (document.querySelector(".saat").innerHTML = "0" + saat)
+  saat < 10
+    ? (document.querySelector(".saat").innerHTML = "0" + saat)
+    : (document.querySelector(".saat").innerHTML = saat);
+
+  if (dakika < 10) {
+    document.querySelector(".dakika").innerHTML = "0" + dakika;
+  } else {
+    document.querySelector(".dakika").innerHTML = dakika;
+  }
+  if (saniye < 10) {
+    document.querySelector(".saniye").innerHTML = "0" + saniye;
+  } else {
+    document.querySelector(".saniye").innerHTML = saniye;
+  }
+  if (saat > 12) {
+    document.querySelector(".gece").innerHTML = "PM";
+  } else {
     document.querySelector(".gece").innerHTML = "AM";
-}
-
-
+  }
 },1000)
 // let saat = new Date().getHours();
 // console.log(saat);
